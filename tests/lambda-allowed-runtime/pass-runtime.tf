1d8cde00644dcf43f6635d4333fee0f98f3a8eb3
Dean Wilson <dean.wilson@gmail.com>
2020-07-30 10:09:24 +0100

    Add the checkov test and some test terraform files.

---
+++ tests/lambda-allowed-runtime/pass-runtime.tf
@@ -0,0 +1,10 @@
+resource "aws_lambda_function" "process_scans" {
+  filename         = var.lambda_zip_location
+  source_code_hash = filebase64sha256(var.lambda_zip_location)
+  function_name    = "sample lambda"
+  role             = aws_iam_role.sample_lambda_role.arn
+  handler          = "sample_lambda.main"
+  runtime          = "python3.8"
+  timeout          = "900"
+  memory_size      = 2048
+}
